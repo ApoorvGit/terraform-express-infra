5818833c5586da0e741a26a30b569e5023f4bf43
karthik01455 <karthikshriram4509@gmail.com>
2023-04-20 11:59:39 +0530

    feat: cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
 const prisma = new PrismaClient();
 
 // Endpoint to add a color to the database
@@ -26,6 +29,25 @@ app.get('/colors', async (req, res) => {
   try {
     const colors = await prisma.color.findMany();
     res.json(colors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint to print hello world
+app.get('/user', async (req, res) => {
+  try {
+    console.log('user');
+    res.json('user');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/user/profile', async (req, res) => {
+  try {
+    console.log('user');
+    res.json('user');
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
